5952851e4542630fc76cbe97e7c342802fbde350
Erriez <Erriez@users.noreply.github.com>
2018-05-25 22:57:41 +0200

    Fix build and library path.

--- platformio/platformio_build.sh
+++ platformio/platformio_build.sh
@@ -8,11 +8,11 @@
 # git pull
 
 # Build example(s)
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/Alarm/Alarm.ino
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/Benchmark/Benchmark.ino
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/GettingStarted/GettingStarted.ino
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/PrintDateTime/PrintDateTime.ino
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/RAM/RAM.ino
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/SetTrickleCharger/SetTrickleCharger.ino
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/SquareWave1Hz/SquareWave1Hz.ino
-platformio ci --lib="." --project-conf=platformio/platformio.ini ../examples/Terminal/SquareWave1Hz.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/Alarm/Alarm.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/Benchmark/Benchmark.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/GettingStarted/GettingStarted.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/PrintDateTime/PrintDateTime.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/RAM/RAM.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/SetTrickleCharger/SetTrickleCharger.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/SquareWave1Hz/SquareWave1Hz.ino
+platformio ci --lib=".." --lib="../examples/Alarm" --project-conf=platformio.ini ../examples/Terminal/SquareWave1Hz.ino
