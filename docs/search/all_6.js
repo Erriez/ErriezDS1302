9615cdeee7ce2aa8b130cbb89f3592f9a71f9b07
Travis CI <travis@travis-ci.org>
2018-08-02 16:04:18 +0000

    Deploy code docs to GitHub Pages Travis build: 14
    Commit: c52d8d11ed581c311aa480458fe3e01292146ea5

--- docs/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['low_20precision_20ds1302_20rtc_20library_20for_20arduino',['Low precision DS1302 RTC library for Arduino',['../index.html',1,'']]]
-];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ds1302_20rtc_20_28real_20time_20clock_29_20library_20for_20arduino',['DS1302 RTC (Real Time Clock) library for Arduino',['../index.html',1,'']]]
+];
